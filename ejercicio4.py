e9ef3b21c014941a194d75c363664d17c6d0ec7e
ednachavez1510-blip <ednachavez1510@gmail.com>
2025-11-20 13:55:36 -0600

    Create ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,13 @@
+# 1. Captura de datos del usuario
+nombre1 = input("Ingresa el primer nombre: ")
+nombre2 = input("Ingresa el segundo nombre: ")
+nombre3 = input("Ingresa el tercer nombre: ")
+nombre4 = input("Ingresa el cuarto nombre: ")
+nombre5 = input("Ingresa el quinto nombre: ")
+
+# 2. Mostrar datos capturados
+print("\n--- DATOS CAPTURADOS ---")
+print("Lista Original:", nombre1, nombre3, nombre2, nombre5, nombre4)
+print("Primer elemento:", nombre1)
+print("Ultimo elemento:", nombre5)
+print("Lista ordenada:", nombre1, nombre2, nombre3, nombre4, nombre5)
