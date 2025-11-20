08e632ec8e940f46f56aae8e6daf17490fc3f9b7
ednachavez1510-blip <ednachavez1510@gmail.com>
2025-11-20 13:43:06 -0600

    Create ejercicio2.py

---
+++ ejercicio2.py
@@ -0,0 +1,13 @@
+num = int(input("Ingresa un número: "))
+
+if num > 0:
+    print("El número es positivo.")
+elif num < 0:
+    print("El número es negativo.")
+else:
+    print("El número es 0.")
+
+if num % 2 == 0:
+    print("El número es par.")
+else:
+    print("El número es impar.")
