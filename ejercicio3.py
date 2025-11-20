522ac9191dcbbf8eb3b50701cc6508b2c196ee26
ednachavez1510-blip <ednachavez1510@gmail.com>
2025-11-20 13:49:39 -0600

    Create ejercicio3.py

---
+++ ejercicio3.py
@@ -0,0 +1,13 @@
+# 1. Captura de numeros
+num1 = float(input("Ingresa el primera numero: "))
+num2 = float(input("Ingresa el segundo numero: "))
+num3 = float(input("Ingresa el tercer numero: "))
+
+# 2. Cálculo del promediov y suma
+promedio = (num1 + num2 + num3) / 3
+suma = (num1 + num2 + num3)
+
+# 3. Mostrar el promedio
+print("Cantidad ingresada :", num1 , num2 , num3)
+print("\nEl promedio es:", promedio)
+print("Suma total: ", suma)
