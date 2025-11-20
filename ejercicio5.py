768184d0dc406284637e0e5484133ce10997fbfb
ednachavez1510-blip <ednachavez1510@gmail.com>
2025-11-20 14:00:19 -0600

    Create ejercicio5.py

---
+++ ejercicio5.py
@@ -0,0 +1,13 @@
+# Programa para calcular el área de un rectangulo
+print("=== Cálculo del área de un rectangulo ===")
+
+# Solicitar el radio del círculo
+base = float(input("Ingresa la base del rectangulo: "))
+altura = float(input("Ingresa la altura del rectangulo: "))
+
+# Calcular el área
+area = base * altura
+
+# Mostrar resultados
+print("\n=== Resultados ===")
+print(f"Área del resctangulo: {area:.2f}")
