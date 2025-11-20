6244408235c444db056b2c2f50591911d8928387
ednachavez1510-blip <ednachavez1510@gmail.com>
2025-11-20 13:39:15 -0600

    Create ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,10 @@
+# 1. Captura de datos del usuario
+nombre = input("Ingresa tu nombre completo: ")
+edad = input("Ingresa tu edad: ")
+carrera = input("Ingresa la carrera que estudias: ")
+
+# 2. Mostrar datos capturados
+print("\n--- DATOS CAPTURADOS ---")
+print("Hola,", nombre)
+print("Estudias", carrera)
+print("y tienes", edad)
